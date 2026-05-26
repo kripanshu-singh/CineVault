6954a92b194e415e72aa21e60dcbb6c670be3160
kripanshu-singh <kripanshusingh16@gmail.com>
2026-05-26 21:46:23 +0530

    Refactor Carousel and related components for improved readability and maintainability
    - Updated Carousel component to enhance structure and readability.
    - Refactored styles in Carousel and CircleRating components for consistency.
    - Cleaned up ContentWrapper and Footer components for better code organization.
    - Ensured consistent formatting across all components and stylesheets.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,20 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.themoviedb.org/3";
+const BASE_URL = "https://my-movie-proxy.kripanshusingh160305.workers.dev/3";
 const TMBD_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
-const headers = {
-    Authorization: "bearer " + TMBD_TOKEN,
-};
-
 const fetchDataApi = async (url, params) => {
-    try {
-        const { data } = await axios.get(BASE_URL + url, {
-            headers,
-            params,
-        });
-        return data;
-    } catch (err) {
-        console.log("fetchDataApi ~ error :- ", err);
-        return err;
-    }
+  try {
+    const { data } = await axios.get(BASE_URL + url, {
+      params: {
+        api_key: TMBD_TOKEN,
+        ...params,
+      },
+    });
+    return data;
+  } catch (err) {
+    console.log("fetchDataApi ~ error :- ", err);
+    return err;
+  }
 };
 export default fetchDataApi;
