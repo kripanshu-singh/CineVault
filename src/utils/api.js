88fc2a0b1ebedfd050919ca312a5464e97ef1878
kripanshu-singh <kripanshusingh160305@gmail.com>
2026-07-11 21:06:52 +0530

    refactor: remove hardcoded TMDB token from API requests to support server-side proxy authentication

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,11 @@
 import axios from "axios";
 
 const BASE_URL = "https://my-movie-proxy.kripanshusingh160305.workers.dev/3";
-const TMBD_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const fetchDataApi = async (url, params) => {
   try {
     const { data } = await axios.get(BASE_URL + url, {
-      params: {
-        api_key: TMBD_TOKEN,
-        ...params,
-      },
+      params,
     });
     return data;
   } catch (err) {
